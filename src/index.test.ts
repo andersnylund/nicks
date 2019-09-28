d018ca92769ddbed1704625d4eb682589ba75fdf
Anders Nylund <anders@andersnylund.com>
2019-09-28 23:02:35 +0300

    Implement logic

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,47 @@
-import { add, hello } from './index';
+import { nickname } from './index';
+import nicknameDefaultImport from './index';
 
-describe('index', () => {
-  it('should return hello', () => {
-    expect(hello()).toEqual('hello');
+describe('nickname', () => {
+  it('should handle default import', () => {
+    expect(nicknameDefaultImport('')).toEqual([]);
   });
-  it('should add two numbers', () => {
-    expect(add(1, 2)).toEqual(3);
+
+  it('should not return anything with an empty string', () => {
+    expect(nickname('')).toEqual([]);
+  });
+
+  it('should handle special latin characters', () => {
+    expect(nickname('á.èíđßöå')).toEqual(['aaeioa', 'aeioaa', 'aeiaei']);
+  });
+
+  it('should give suggestion based on one name', () => {
+    expect(nickname('Matti')).toEqual(['mmatti', 'mattim', 'matmat']);
+  });
+
+  it('should give suggestions based on two names', () => {
+    expect(nickname('Matti Meikäläinen-Muukalainen')).toEqual([
+      'mmeika',
+      'meikam',
+      'matmei',
+      'meimat',
+    ]);
+  });
+
+  it('should handle double spaces', () => {
+    expect(nickname('Matti  Meikäläinen')).toEqual([
+      'mmeika',
+      'meikam',
+      'matmei',
+      'meimat',
+    ]);
+  });
+
+  it('should handle more than two names', () => {
+    expect(nickname('Matti Meikäläinen Muukalainen')).toEqual([
+      'mmuuka',
+      'muukam',
+      'matmuu',
+      'muumat',
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-export const hello = (): string => 'hello';
+const normalize = (name: string): string =>
+  name
+    .toLowerCase()
+    .trim()
+    .normalize('NFD') // decompose: è => e`
+    .replace(/[\u0300-\u036f]/g, '') // remove special characters
+    .replace(/[^a-zA-Z ]/g, '') // remove non-numbers and non-letters
+    .replace(/\s{2,}/g, ' '); // remove extra spaces
 
-export const add = (a: number, b: number): number => a + b;
+export const nickname = (name: string) => {
+  let suggestions: string[] = [];
+  const normalizedName = normalize(name);
+  const parts: string[] = normalizedName.split(' ');
+  const firstAndLastName = [parts[0], parts[parts.length - 1]];
+  if (firstAndLastName.includes('')) {
+    suggestions = [];
+  } else {
+    const firstNameFirstChar = firstAndLastName[0][0];
+    const lastNameFirst5Chars = firstAndLastName[1].substring(0, 5);
+    const firstNameFirst3Chars = firstAndLastName[0].substring(0, 3);
+    const lastNameFirst3Chars = firstAndLastName[1].substring(0, 3);
+    suggestions = Array.from(
+      new Set(
+        [
+          firstNameFirstChar + lastNameFirst5Chars,
+          lastNameFirst5Chars + firstNameFirstChar,
+          firstNameFirst3Chars + lastNameFirst3Chars,
+          lastNameFirst3Chars + firstNameFirst3Chars,
+        ].filter(suggestion => suggestion.length >= 3)
+      ).values()
+    );
+  }
+  return suggestions;
+};
+
+export default nickname;
